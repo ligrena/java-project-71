c949321a6048f901ba671154a91eeb233c48de98
e.sakhapova <e.sakhapova@tinkoff.ru>
2024-09-30 15:46:37 +0300

    Add new module project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,10 @@ plugins {
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
+application {
+    mainClass = "hexlet.code.App"
+}
+
 repositories {
     mavenCentral()
 }
@@ -13,6 +17,10 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
 }
 
 tasks.getByName<Test>("test") {
